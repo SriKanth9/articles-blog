819ee5694e9054dec09891861f655b806734e0fd
srikanth K <srikanth.c.kongara@gmail.com>
2020-09-29 00:59:01 +0530

    First commit

---
+++ src/build/precache-manifest.7d087ee2cab5073b8f5c343e24d7ec8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62705b71d18ce2484b212915e1fafd16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4825d8fdb090427463d9",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "0a1eee7635066004ba43",
+    "url": "/static/js/2.794ff597.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.794ff597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4825d8fdb090427463d9",
+    "url": "/static/js/main.fa022181.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
